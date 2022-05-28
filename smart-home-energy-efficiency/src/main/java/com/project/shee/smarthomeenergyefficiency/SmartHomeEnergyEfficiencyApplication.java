12664ac8b92cdb6b27dd7af258edb0ed0d507e15
kuzicnenad <kuzic_nenad@protonmail.com>
2022-05-28 11:34:13 +0200

    spring initializr; maven, boot v2.6.8, jar, java18

---
+++ smart-home-energy-efficiency/src/main/java/com/project/shee/smarthomeenergyefficiency/SmartHomeEnergyEfficiencyApplication.java
@@ -0,0 +1,13 @@
+package com.project.shee.smarthomeenergyefficiency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHomeEnergyEfficiencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHomeEnergyEfficiencyApplication.class, args);
+	}
+
+}
